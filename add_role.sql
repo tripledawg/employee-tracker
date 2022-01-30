277d9d2719f803f8936cd2830f9d5ea266d5184e
tripledawg <iamlesliecrouch>
2022-01-30 00:03:23 -0500

    added missing query, corrected typos

--- add_role.sql
+++ add_role.sql
@@ -1,6 +1,4 @@
 USE employees_db;
-INSERT INTO employee (employee_role_id, first_name, last_name, manager_id)
-SELECT er.id, ?, ?, e.id FROM employee e
-LEFT JOIN employee_role er on ? = er.title
-WHERE e.first_name = ? AND e.last_name = ?;
+INSERT INTO employee_role (title, salary, department_id)
+VALUE (?, ?, (SELECT d.id FROM department d WHERE department_name = ?));
 
